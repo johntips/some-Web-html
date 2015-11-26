759c8f9bf59f9c0139f33c03dffff22788b16765
Teiyu Ueki <tu.ue.et@gmail.com>
2015-11-26 15:07:54 +0900

    added gif

--- js/index.js
+++ js/index.js
@@ -1,38 +1,80 @@
-var openPhotoSwipe = function() {
-    var pswpElement = document.querySelectorAll('.pswp')[0];
-    
-    // build items array
-    var items = [
-        {
-            src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
-            w: 1024,
-            h: 683
-	},
-        {
-            src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
-            w: 1024,
-            h: 683
+    function openGarraly(obj){
+
+
+        var styles = getComputedStyle(obj);
+        var id = String(obj.id);
+
+        var openPhotoSwipe = function() {
+        var pswpElement = document.querySelectorAll('.pswp')[0];
+
+        // build items array
+        var items =[]
+        var hash = {};
+        var styles = getComputedStyle(obj);
+        var id = String(obj.id);
+
+        //garraryに渡す、gararry番号、gararry内画像番号、gararry内画像番号配列、
+        var gallerysCount = id.replace(/img(.+)-(.+)/,"$1");
+        var galleryImgsCount = id.replace(/img(.+)-(.+)/,"$2");
+        var setGoto = galleryImgsCount - 1;
+        var garraryUrlArray = [];
+        var length = obj.parentNode.children.length;
+        //親クラスの特定、
+        var garraryElements = obj.parentNode.children;
+
+
+        console.log(length);
+
+        for(var i=0,l= length; i < l; i++) {
+            // figureEl = galleryElements[i];
+            // linkEl = figureEl.children[0];
+            // size = linkEl.getAttribute("data-size").split("x");
+            var tmpurl  = garraryElements[i].style.backgroundImage;
+            var settitle =  garraryElements[i].innerText;
+            console.log(settitle);
+
+            var seturl = tmpurl.replace(/^url\(|\)/g, "");
+            console.log(seturl);
+
+            items.push({
+                src: seturl,
+                w: 1000,
+                h: 1000,
+                msrc: seturl,
+                // el: figureEl,
+                title: settitle
+            });
+
         }
-    ];
-    
-    // define options (if needed)
-    var options = {
-			 // history & focus options are disabled on CodePen        
-      	history: false,
-      	focus: false,
 
-        showAnimationDuration: 0,
-        hideAnimationDuration: 0
-        
+
+        console.log(items);
+        var options = {
+            showAnimationDuration: 300,
+            hideAnimationDuration: 300,
+            history: false,
+            focus: false,
+            index: setGoto,
+        };
+
+        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
     };
-    
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-    
 
-    gallery.init();
-    gallery.goTo(1);
-};
+    openPhotoSwipe();
+    }
 
-openPhotoSwipe();
-
-document.getElementById('swiper-slide').onclick = openPhotoSwipe;
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflow: {
+            rotate: 10,
+            stretch: 20,
+            depth: 190,
+            modifier: 1,
+            slideShadows : true
+        }
+    });
